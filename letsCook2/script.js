236be3e86733d3a4d7141bc7a383a63e8dc206ea
Theo03iou <75097082+Theo03iou@users.noreply.github.com>
2022-03-03 22:30:35 +0000

    Add files via upload

---
+++ letsCook2/script.js
@@ -0,0 +1,150 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// Event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+// Dark Mode
+
+document.querySelector('#dark-btn').addEventListener('click', darkMode);
+
+function darkMode() {
+    let darkModeBody = document.querySelector('body')
+    darkModeBody.classList.toggle('dark')
+
+    let darkModeSearch = document.querySelector('.search-control')
+    darkModeSearch.classList.toggle('darkBar')
+
+}
+
+// Previous attempts at dark mode
+
+// let darkModeItem = document.querySelector('.meal-item')
+    // darkModeItem.classList.toggle('grey')
+
+
+// $(document).ready(function () {
+//     $('#dark-btn').click(function () { 
+//         $('body').toggleClass('dark');
+//         console.log('works');
+//     });
+        
+// });
+
+// $(document).ready(function () {
+//     $('#dark-btn').click(function () { 
+//         $('body').addClass('dark');
+//         console.log('frerfrfer');
+//     });
+// });
+
+
+// Get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log('success')
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            // This is the part which handles invalid inputs
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
+
+
+// Get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+// Create a model
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
+
+// I'm Feeling LUCKY!!
+
+document.querySelector('#ifl-btn').addEventListener('click', imFeelingLucky);
+
+function imFeelingLucky(){
+    var ingredients = ["egg","potato","pork","chicken", "onion", "flour"];
+    var randIngredients = ingredients[Math.floor(Math.random() * ingredients.length)];
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${randIngredients}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log('success')
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            // This is the part which handles invalid inputs
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
